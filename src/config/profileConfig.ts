4a5850d337ae2416985de4f5d64b6dc9e94d8012
q485707079-source <Hikamishiori@outlook.com>
2026-06-28 03:43:27 +0800

    文章整理

--- src/config/profileConfig.ts
+++ src/config/profileConfig.ts
@@ -9,10 +9,10 @@ export const profileConfig: ProfileConfig = {
 	avatar: "assets/images/Image_69348356919377(1)..avif",
 
 	// 名字
-	name: "IkegakiKazuo",
+	name: "Kazuo",
 
 	// 个人签名
-	bio: "Hello, I'm IkegakiKazuo.",
+	bio: "K.A.Z.U.O.",
 
 	// 链接配置
 	// 已经预装的图标集：fa7-brands，fa7-regular，fa7-solid，material-symbols，simple-icons
@@ -22,10 +22,10 @@ export const profileConfig: ProfileConfig = {
 	// showName: true 时显示图标和名称，false 时只显示图标
 	links: [
 		{
-			name: "qq",
+			name: "3010327806",
 			icon: "fa7-brands:qq",
-			url: "https://qm.qq.com/q/ZGsFa8qX2G",
-			showName: false,
+			url: "#",
+			showName: true,
 		},
 		{
 			name: "GitHub",
@@ -36,7 +36,7 @@ export const profileConfig: ProfileConfig = {
 		{
 			name: "Email",
 			icon: "fa7-solid:envelope",
-			url: "mailto:xiaye@msn.com",
+			url: "mailto:3010327806@qq.com",
 			showName: false,
 		},
 		{
